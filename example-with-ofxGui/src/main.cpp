b8465cc192ccd823a34a3f794277b1d42799fa26
d3cod3 <n3m3da@d3cod3.org>
2020-08-01 10:59:30 +0200

    added glfw window settings

--- example-with-ofxGui/src/main.cpp
+++ example-with-ofxGui/src/main.cpp
@@ -1,12 +1,23 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+#include "ofAppGLFWWindow.h"
+
 //========================================================================
 int main( ){
     ofGLWindowSettings settings;
     settings.setGLVersion(3, 2); // Using programmable renderer. Comment out this line to use the 'standard' GL renderer.
-    settings.width = 1024;
-    settings.height = 768;
-    ofCreateWindow(settings);
-    ofRunApp(new ofApp());
+
+    settings.setSize(1024,768);
+
+    shared_ptr<ofAppGLFWWindow> mainWindow = dynamic_pointer_cast<ofAppGLFWWindow>(ofCreateWindow(settings));
+    shared_ptr<ofApp> mainApp(new ofApp);
+
+    ofRunApp(mainWindow, mainApp);
+
+    ofRunMainLoop();
+
+    // done
+    return EXIT_SUCCESS;
+
 }
